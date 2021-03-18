abc3936dbd0eeed3829cd2c198c97025c0b50a7f
parsifal <1nibob1@gmail.com>
2021-03-18 17:44:24 +0200

    Almost fixed the issue with the condition when we are asking if there is another one user or not

--- the_secret_auction_app.py
+++ the_secret_auction_app.py
@@ -17,34 +17,49 @@ logo = '''
 
 bids_log = []
 different_bidder = True
-
+result = []
+test = True
+# def highest_bid(new_name, new_bid):
+#     new_dict = {"name": new_name, "bid": new_bid}
+#     bids_log.append(new_dict)
+#     top_bid = 0
+#     for value in bids_log:
+#         if int(value['bid']) > top_bid:
+#             top_bid = int(value['bid'])
+#     return top_bid
 
 def highest_bid(new_name, new_bid):
     new_dict = {"name": new_name, "bid": new_bid}
-    print(f"new dict: {new_dict}")
     bids_log.append(new_dict)
-    print(f"bids log: {bids_log}")
-    print(f"bid: {bids_log[0]['bid']}")
-    top_bid = 0
-    for bid in bids_log:
-        if int(bid['bid']) > top_bid:
-            top_bid = int(bid['bid'])
-    print(f"THe highest_bid is {top_bid}")
+    top_bid = [0, '', ]
+    for value in bids_log:
+        if int(value['bid']) > int(top_bid[0]):
+            top_bid[0] = int(value['bid'])
+            top_bid[1] = value['name']
+    return top_bid
 
 
-# print(logo)
+print(logo)
 print("Welcome to the secret auction program.")
 
 while different_bidder:
     name = input("What is your name?: ")
     bid = input("What is your bid?: ")
-    highest_bid(new_name=name, new_bid=bid)
-    condition = input("Are there any other bidders? Type 'yes' or 'no': ")
-    if condition == 'no':
-        different_bidder = False
-        print("Bye")
-    elif condition == 'yes':
-        print("okido")
-        different_bidder = True
-    # else:
-    #     print("Please, try again.")
+    result = highest_bid(new_name=name, new_bid=bid)
+    print(f'result is {result}')
+    while test:
+        condition = input("Are there any other bidders? Type 'yes' or 'no': ")
+        if condition == 'no':
+            different_bidder = False
+            test = False
+            print(f"The highest bid is {result[0]}. Congrats {result[1]} with it!")
+            print("Bye")
+        elif condition == 'yes':
+            print("okido")
+            different_bidder = True
+            test = False
+        else:
+            continue
+
+
+
