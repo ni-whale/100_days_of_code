d6cf61c58b21647d4cca828f26b1a4bf55066091
parsifal <1nibob1@gmail.com>
2021-03-30 15:18:11 +0300

    Alternative code with the same solution

--- Calculator/main.py
+++ Calculator/main.py
@@ -26,29 +26,59 @@ operations = {
     '/': divide
 }
 
+# num1 = int(input("What's the first number?: "))
+# num2 = int(input("What's the second number?: "))
+#
+# for symbol in operations:
+#     print(symbol)
+# operation_symbol = input("Pick an operation from the line above: ")
+
+# function = operations[operation_symbol]
+# answer = function(n1=num1, n2=num2)
+# print(f"{num1} {operation_symbol} {num2} = {answer}")
+# condition = True
+# while condition:
+#     if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == 'y':
+#         operation_symbol = input("Pick an another operation: ")
+#         num3 = int(input("What is the next number?: "))
+#         function = operations[operation_symbol]
+#         next_answer = function(answer, num3)
+#         print(f"{answer} {operation_symbol} {num3} = {next_answer}")
+#         answer = next_answer
+#     else:
+#         print("Good bye!")
+#         condition = False
+
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+
 
 for symbol in operations:
     print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+should_continue = True
+symbol_check = True
 
-function = operations[operation_symbol]
-first_answer = function(n1=num1, n2=num2)
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-condition = True
-while condition:
-    the_next_action = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit: ")
-    if the_next_action == 'y':
-        operation_symbol = input("Pick an another operation: ")
-        num3 = int(input("What is the next number?: "))
-        function = operations[operation_symbol]
-        second_answer = function(first_answer, num3)
-        print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
-        first_answer = second_answer
+while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = int(input("What's the next number?: "))
+    function = operations[operation_symbol]
+    answer = function(n1=num1, n2=num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == 'y':
+        num1 = answer
     else:
-        print("Good bye!")
-        condition = False
+        print("Good buy")
+        should_continue = False
+
+
+
+
+
+
+
+
+
+
 
 
 
