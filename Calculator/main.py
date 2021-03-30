857dcfa60608ddbb49ae045608dd9c57e018566b
parsifal <1nibob1@gmail.com>
2021-03-30 07:02:15 +0300

    Basic calc operations.

---
+++ Calculator/main.py
@@ -0,0 +1,38 @@
+# Add
+def add(n1, n2):
+    """Summarise 2 different digits"""
+    return n1 + n2
+
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+
+function = operations[operation_symbol]
+answer = function(n1=num1, n2=num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
