64436d6f5c41a1d9a6ef613772a8cfe6ae80ba55
parsifal <1nibob1@gmail.com>
2021-03-30 13:16:26 +0300

    Made a loop which allows to work with the last result in the new the iteration.

--- Calculator/main.py
+++ Calculator/main.py
@@ -34,5 +34,25 @@ for symbol in operations:
 operation_symbol = input("Pick an operation from the line above: ")
 
 function = operations[operation_symbol]
-answer = function(n1=num1, n2=num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+first_answer = function(n1=num1, n2=num2)
+print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+condition = True
+while condition:
+    the_next_action = input(f"Type 'y' to continue calculating with {first_answer}, or type 'n' to exit: ")
+    if the_next_action == 'y':
+        operation_symbol = input("Pick an another operation: ")
+        num3 = int(input("What is the next number?: "))
+        function = operations[operation_symbol]
+        second_answer = function(first_answer, num3)
+        print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        first_answer = second_answer
+    else:
+        print("Good bye!")
+        condition = False
+
+
+
+
+
+
+
