f10fb98d5f2cef82e2f4c7ff464bd113e5d6fe6d
parsifal <1nibob1@gmail.com>
2021-04-01 19:57:24 +0300

    Added comments to make the code more readable. Couldn't fix the bug where when you decline to take another card the app is finish the work.

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -2,10 +2,10 @@ import art
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-condition = True
-flag = True # if flag == 0 it's a dealer, if 1 - user
+
 
 def new_hand():
+    """Using only for the start of the game to generate the hand for the player/dealer"""
     hand = []
     for card in range(2):
         hand.append(new_card())
@@ -13,11 +13,13 @@ def new_hand():
 
 
 def new_card():
+    """Using for getting a new card from the deck"""
     card = random.choice(cards)
     return card
 
 
 def score(hand):
+    """Showing the current score for the one player"""
     new_score = 0
     for value in hand:
         new_score += value
@@ -25,60 +27,75 @@ def score(hand):
 
 
 def check(score):
-    if score == 21:
-        return 'ok'
-    elif score > 21:
+    """Simple check which shows if the user can continue the game"""
+    if score > 21:
         return 'Bust!'
     else:
         return 'ok'
 
 
-def print_current_state(player_hand, dealer_hand, condition):
-    if condition:
+def print_current_state(player_hand, dealer_hand, needs_another_card):
+    """Printing the result depending on users needs to take a new card"""
+    if needs_another_card:
         print(f'Your cards: {player_hand}, current score is {score(player_hand)}')
         print(f"Dealer's first card is {dealer_hand[0]}")
     else:
         print(f'Your cards: {player_hand}, current score is {score(player_hand)}')
         print(f"Dealer's cards {dealer_hand}, final score is {score(dealer_hand)}")
-    # need to change this print out. It will be a bullshit when I will need to count the score for the dialler
 
 
 def game():
     if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
-        # print(art.logo)
+        print(art.logo)
+        # Start of the game where player and dialer are getting initial cards and we see the current score
         player_hand = new_hand()
         dealer_hand = new_hand()
         print_current_state(player_hand, dealer_hand, True)
         print(check(score(player_hand)))
-        flag = True
-        while flag:
+        # End of this section
+
+        # Asking the user if he want to take another card and checking his score after this action
+        should_take_another_card = True
+        while should_take_another_card:  # loop which allows the user to take as many cards as required
             if input("Type 'y' to get another card, type 'n' to pass: ") == 'y':
                 player_hand.append(new_card())
                 print_current_state(player_hand, dealer_hand, True)
-                if check(score(player_hand)) != 'ok':
+                if check(score(player_hand)) == 'Bust!':
+                    print_current_state(player_hand, dealer_hand, False)
                     print('You lose!')
-                    exit()
+                    game()
                 else:
-                    break
+                    continue
             else:
-                print("lol")
-                flag = False
+                should_take_another_card = False
+                continue
+        # End of this section
 
+        # Checking dealer's score and giving him another card if score less than 12
         if (score(dealer_hand)) < 12:
             dealer_hand.append(new_card())
             print_current_state(player_hand, dealer_hand, False)
-        else:
-            if check(score(dealer_hand)) == 'ok':
-                if score(player_hand) > score(dealer_hand):
-                    print('You win!')
-                else:
-                    print('You lose!')
-            else:
+            if check(score(dealer_hand)) == 'Bust!':
+                print_current_state(player_hand, dealer_hand, False)
                 print('You win!')
                 game()
+        # End of this section
+
+        # else:
+        #     if check(score(dealer_hand)) == 'ok':
+        #         if score(player_hand) > score(dealer_hand):
+        #             print_current_state(player_hand, dealer_hand, False)
+        #             print('You win!')
+        #         else:
+        #             print('You lose!')
+        #             print_current_state(player_hand, dealer_hand, False)
+        #     else:
+        #         print('You win!')
+        #         game()
 
     else:
         print('Good buy!')
+        exit()
 
 
 game()
