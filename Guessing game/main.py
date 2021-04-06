447a6e773583867490a7dcd9dd5e1ea40a053364
parsifal <1nibob1@gmail.com>
2021-04-06 17:53:56 +0300

    Minor changes in game()

--- Guessing game/main.py
+++ Guessing game/main.py
@@ -18,18 +18,36 @@ def checking_the_guess(secret_number, user_guess):
     if secret_number == user_guess:
         return 0  # with this option the game will finish
     elif secret_number > user_guess:
-        return 'Too high!'
-    elif secret_number < user_guess:
         return 'Too low!'
+    elif secret_number < user_guess:
+        return 'Too high!'
     else:
         return 1  # with this option the user will need to try guess again
 
 
 def game():
+    print(art.logo)
     print("Welcome to the Guessing Game!")
-    print("I'm thinking of a number between 1 and 100")
+    print("-----------------------------")
+    print("I'm thinking of a number between 1 and 100...")
     secret_number = random.randrange(1, 100)
-    # print(f"Generated number is {secret_number}")
+    print(f"Generated number is {secret_number}")
+    typo_is_here = True
+    while typo_is_here:
+        difficulty = input("Choose difficulty. Type 'easy' or 'hard': ")
+        typo_is_here = False if difficulty == 'easy' or difficulty == 'hard' else "Try again, dear."  # else statement doesn't printing for some reason...
+    lives = number_of_lives(difficulty)
+    print(f"You have {lives} attempts remaining to guess the number.")
+    typo_is_here = True
+    while typo_is_here:
+        user_guess = int(input("Make a guess: "))
+        if checking_the_guess(secret_number, user_guess) == 0:
+            print(f"You got it! The answer is {secret_number}")
+            exit()
+        elif checking_the_guess(secret_number, user_guess) == 1:
+            print("Try again, dear.")
+        else:
+            print(checking_the_guess(secret_number, user_guess))
 
 
 game()
