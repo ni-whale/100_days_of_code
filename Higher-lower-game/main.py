ecfb373e45037d534a84e6f0fdd8f929b85f2a6c
parsifal <1nibob1@gmail.com>
2021-04-12 06:53:01 +0300

    Final version.

--- Higher-lower-game/main.py
+++ Higher-lower-game/main.py
@@ -4,7 +4,10 @@ import random
 
 
 log = []
+score = 0
+winner = ""
 the_game_is_end = False
+typo_in_game = True
 
 
 def random_option():
@@ -25,20 +28,36 @@ def info_printing(selected_option):
 
 def compare_options(selected_option_1, selected_option_2):
     if selected_option_1['follower_count'] > selected_option_2['follower_count']:
-        ...
-
+        return "A", selected_option_1
+    else:
+        return "B", selected_option_2
 
 
 print(art.logo)
 while not the_game_is_end:
-    selected_option_1 = random_option()
+    if score == 0:
+        selected_option_1 = random_option()
+    else:
+        selected_option_1 = winner
     selected_option_2 = random_option()
     print(f"Compare A: {info_printing(selected_option_1)}")
     print(art.vs)
     print(f"Compare B: {info_printing(selected_option_2)}")
-    the_game_is_end = True
-
-
-# print(random_option())
-
-# info_printing(random_option())
+    while typo_in_game:
+        try:
+            guess = input("Who had more followers? Type 'A' or 'B': ")
+            if guess.upper() == "A" or guess.upper() == "B":
+                break
+        except NameError("A"):  # Not sure which exception should be there to print the result. Need to google it.
+            print("Oops! That was not valid input. Try again...")
+    result_of_compare, winner = compare_options(selected_option_1, selected_option_2)
+    if result_of_compare == guess.upper():
+        score += 1
+        print("---------------------------------")
+        print(f"You're right! Current score: {score}")
+        print("---------------------------------")
+    else:
+        print("---------------------------------")
+        print(f"Sorry, that's wrong. Your final score: {score}")
+        print("---------------------------------")
+        the_game_is_end = True
