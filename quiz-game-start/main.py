927a0efa9db0c0c2d61ecb4c65921c04fe4c3bba
parsifal <1nibob1@gmail.com>
2021-05-06 16:23:04 +0300

    Trying to solve still_has_question function

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,9 +1,17 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for record in question_data:
     question_bank.append(Question(record["text"], record["answer"]))
 
-print(question_bank[0].answer)
+quiz_brain = QuizBrain(question_bank)
+
+in_game = True
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
+
--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -2,6 +2,16 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+        self.showed_questions = []
 
-    def next_question(self, q_number):
-        ...
+    def still_has_questions(self):
+        for element in self.question_list:
+            if self.question_list[element] == self.showed_questions[element]:
+                print("Found the question which appeared already")
+                return False
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        self.showed_questions.append(self.question_list[self.question_number])
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
