8c49e8ac01d3f98a4f86c7bf610e10e9a5d89cec
parsifal <1nibob1@gmail.com>
2021-05-27 08:03:45 +0300

    Trying to figure out how to work with multiple Turtle object the same time.

--- Turtle race/main.py
+++ Turtle race/main.py
@@ -1,21 +1,31 @@
 from turtle import Turtle, Screen
 from random import choice
 
-
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 print(user_bet)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-names = ["michelangelo", "raphael", "donatello", "leonardo", "shredder"]
 turtles = []
 i = -150
 
-for turtle in names:
-    turtle = Turtle(shape="turtle")
+t1 = Turtle(shape="turtle")
+t2 = Turtle(shape="turtle")
+t3 = Turtle(shape="turtle")
+t4 = Turtle(shape="turtle")
+t5 = Turtle(shape="turtle")
+turtles.append(t1)
+turtles.append(t2)
+turtles.append(t3)
+turtles.append(t4)
+turtles.append(t5)
+
+
+for turtle in turtles:
     turtle.color(choice(colors))
     turtle.penup()
-    turtle.goto(-230, i+20)
+    i = i+50
+    turtle.goto(-230, i)
 
 print(turtles)
 
