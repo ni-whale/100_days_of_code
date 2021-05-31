11d8a261ded14ad3ef733b26f84694f6516f1ac1
parsifal <1nibob1@gmail.com>
2021-05-31 07:05:59 +0300

    Starting a new project "Snake game"

---
+++ snake_game/main.py
@@ -0,0 +1,22 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
