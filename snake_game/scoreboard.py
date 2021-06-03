1e219f4ec6cc87523612af320dd4fbc0ceba5233
parsifal <1nibob1@gmail.com>
2021-06-03 07:29:24 +0300

    Extending functionality of the Snake Game. Added a working scoreboard, a tail growing and a wall collision.

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 18, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,8 +10,17 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=270)
         self.hideturtle()
-        test = "kek"
-        self.write(f"Score = {test}", align="center", font=("Arial", 15, "normal"))
+        self.score = 0
+        self.update_scoreboard()
 
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
