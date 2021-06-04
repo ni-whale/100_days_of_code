b8916fc758326af8e921638d2f7572bf2495d525
parsifal <1nibob1@gmail.com>
2021-06-04 07:13:49 +0300

    Finished the game.

--- snake_game/main.py
+++ snake_game/main.py
@@ -39,16 +39,12 @@ while game_is_on:
         scoreboard.game_over()
 
     #Detect collision with the tale
-
-
-
-
-
-
-
-
-
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -9,9 +9,9 @@ RIGHT = 0
 
 class Snake:
     def __init__(self):
-        self.snake_segments = []
+        self.segments = []
         self.create_snake()
-        self.head = self.snake_segments[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,16 +22,16 @@ class Snake:
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
-        self.snake_segments.append(new_segment)
+        self.segments.append(new_segment)
 
     def extend(self):
-        self.add_segment(self.snake_segments[-1].position())
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.snake_segments) - 1, 0, -1):
-            new_x = self.snake_segments[seg_num - 1].xcor()
-            new_y = self.snake_segments[seg_num - 1].ycor()
-            self.snake_segments[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
