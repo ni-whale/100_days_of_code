836e19996a985728ad54573a6c0a1bed80c93f51
parsifal <1nibob1@gmail.com>
2021-06-28 06:31:32 +0300

    Snake_game was a bit updated by adding a "High score"

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -11,14 +11,23 @@ class Scoreboard(Turtle):
         self.goto(x=0, y=270)
         self.hideturtle()
         self.score = 0
+        self.high_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.home()
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.score = 0
+            self.update_scoreboard()
+
+
+    # def game_over(self):
+    #     self.home()
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
