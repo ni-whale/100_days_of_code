45666af13a1e6741b4090f665cebbbac37df6314
parsifal <1nibob1@gmail.com>
2021-07-01 14:02:19 +0300

    DB with central park squirrels and 2 ways to count sq with the specify fur color.

--- small_projects/day-25.py
+++ small_projects/day-25.py
@@ -14,7 +14,7 @@ import csv
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 
 # temp_list = data["temp"].to_list()
 
@@ -24,6 +24,35 @@ data = pandas.read_csv("weather_data.csv")
 
 # print(data[data.temp == data.temp.max()])
 
-day = data[data.day == "Monday"]
+# day = data[data.day == "Monday"]
+#
+# print(int(day.temp) * 1.8 + 32)
 
-print(int(day.temp) * 1.8 + 32)
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+i = 0
+
+# gray = data["Primary Fur Color"] == "Cinnamon"
+# print(gray.count())
+
+
+# data_set = data["Primary Fur Color"].to_list()
+# data_dict = {
+#     "Fur Color": ["Gray", "Red", "Black"],
+#     "Count": [0, 0, 0]
+# }
+#
+# for value in data_set:
+#     if value == "Gray":
+#         data_dict["Count"][0] += 1
+#     elif value == "Cinnamon":
+#         data_dict["Count"][1] += 1
+#     elif value == "Black":
+#         data_dict["Count"][2] += 1
+#
+# new_data = pandas.DataFrame(data_dict)
+# new_data.to_csv("squirrel_count")
+
+# grey_squirrel = len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrel = len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrel = len(data[data["Primary Fur Color"] == "Black"])
