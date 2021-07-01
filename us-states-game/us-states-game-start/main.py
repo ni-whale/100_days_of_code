09bbf7dff6cfa9f6ecde14b4b44469818d2eaeec
parsifal <1nibob1@gmail.com>
2021-07-01 15:34:52 +0300

    Based structure of the state_guess game.

---
+++ us-states-game/us-states-game-start/main.py
@@ -0,0 +1,19 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+
+# def get_mouse_click_coordinates(x, y):
+"""Code to find out x, y location based on the U.S.A. map attached to this project"""
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coordinates)
+# turtle.mainloop()
+
