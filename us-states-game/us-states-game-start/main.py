37de2cbe8f1701bc3a285c1f157c5585332783df
parsifal <1nibob1@gmail.com>
2021-07-01 17:45:39 +0300

    Working with comparing data from csv and user's input.

--- us-states-game/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/main.py
@@ -1,12 +1,23 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(image)
+# turtle.shape(image)
 
+game_is_on = True
 
+data = pandas.read_csv("50_states.csv")
+guessed_states = []
+
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+    if data.state.:
+        guessed_states.append(answer_state)
+
+    print(guessed_states)
 
 
 
