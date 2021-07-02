d261a47ef23a4e5054640ff059d469eb618787e6
parsifal <1nibob1@gmail.com>
2021-07-02 15:42:57 +0300

    Testing different ways to get info from scv by pandas.

--- us-states-game/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/main.py
@@ -1,21 +1,48 @@
 import turtle
 import pandas
 
+""" 
+- Convert the guess to Title case
+    - Check if the guess is among the 50 states
+    - Write correct guesses onto the map
+    - Use a loop to allow the user to keep guessing
+    - Record the correct guesses in a list
+    - Keep track of the score     
+"""
+
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-# turtle.shape(image)
+turtle.shape(image)
 
 game_is_on = True
 
 data = pandas.read_csv("50_states.csv")
 guessed_states = []
 
+# while game_is_on:
+#     answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+#     for state in data.state:
+#         if str(state).lower() == answer_state.lower():
+#             guessed_states.append(answer_state)
+
+# while game_is_on:
+#     answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+#     temp = data[data.state == answer_state]
+#     guessed_states.append(temp)
+
 while game_is_on:
     answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
-    if data.state.:
-        guessed_states.append(answer_state)
+    for df in data:
+        # if str(df.state).lower() == answer_state.lower():
+        #     guessed_states.append(answer_state)
+        print(df.state)
+
+
+
+
 
     print(guessed_states)
 
