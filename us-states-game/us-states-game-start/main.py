bf6501209690ae562bac1f79a6df891e1b52ea88
parsifal <1nibob1@gmail.com>
2021-07-03 08:15:00 +0300

    Added keeping track of correct answers of the user.

--- us-states-game/us-states-game-start/main.py
+++ us-states-game/us-states-game-start/main.py
@@ -10,7 +10,7 @@ import pandas
     - Keep track of the score     
 """
 
-FONT = ("Courier", 10, "normal")
+FONT = ("Courier", 12, "normal")
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -22,33 +22,27 @@ t = turtle.Turtle()
 t.penup()
 t.hideturtle()
 
-game_is_on = True
-
 data = pandas.read_csv("50_states.csv")
 guessed_states = []
 
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+
+game_is_on = True
 while game_is_on:
-    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
     for state in data.state:
         if str(state).lower() == answer_state.lower():
-            guessed_states.append(answer_state)
+            guessed_states.append(state)
             current_state = data[data.state == state]  # saving the filtered output to get access to x, y
             t.goto(float(current_state.x), float(current_state.y))
             t.write(f"{state}", font=FONT)
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?")  # updating title to keep correct answers
 
-
+# Another variant:
 # while game_is_on:
 #     answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
 #     temp = data[data.state == answer_state]
 #     guessed_states.append(temp)
-
-
-
-
-
-
-    print(guessed_states)
-
 
 
 # def get_mouse_click_coordinates(x, y):
@@ -57,4 +51,3 @@ while game_is_on:
 #
 # turtle.onscreenclick(get_mouse_click_coordinates)
 # turtle.mainloop()
-
