154276a22cbc0b6866250aa71169ef29fd3e6282
parsifal <1nibob1@gmail.com>
2021-07-09 07:19:39 +0300

    Trying dict of comprehension in use

--- small_projects/day-26/main.py
+++ small_projects/day-26/main.py
@@ -7,14 +7,41 @@
 #
 # print(result)
 
-with open("file1.txt") as file1:
-    list1 = file1.readlines()
+# with open("file1.txt") as file1:
+#     list1 = file1.readlines()
+#
+# with open("file2.txt") as file2:
+#     list2 = file2.readlines()
+#
+# result = [int(num) for num in list1 if num in list2]
+#
+# # Write your code above 👆
+# print(result)
 
-with open("file2.txt") as file2:
-    list2 = file2.readlines()
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# # Don't change code above 👆
+#
+# # Write your code below:
+# result = {word: len(word) for word in sentence.split()}
+#
+# print(result)
 
-result = [int(num) for num in list1 if num in list2]
 
-# Write your code above 👆
-print(result)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
 
+# (temp_c * 9/5) + 32 = temp_f
+
+# Write your code 👇 below:
+weather_f = {day: temp_c * 1.8 + 32 for day, temp_c in weather_c.items()}
+
+
+print(weather_f)
