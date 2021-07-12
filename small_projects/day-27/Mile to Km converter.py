e9d1b367c2a587f0b0ec77934946bb3e7603195f
parsifal <1nibob1@gmail.com>
2021-07-12 06:12:58 +0300

    Finished with GUI converter

--- small_projects/day-27/Mile to Km converter.py
+++ small_projects/day-27/Mile to Km converter.py
@@ -4,17 +4,18 @@ FONT = ("Courier", 10, "normal")
 
 
 def button_cliked():
-    my_label.config(text=input.get())
+    value = entry.get()
+    result = float(value) * 1.609
+    label2.config(text=result)
 
 
 window = Tk()
 window.title("Mike to Km converter")
 window.minsize(width=300, height=100)
-window.config(padx=10, pady=10)
+window.config(padx=20, pady=20)
 
-
-# Entrie
-entry = Entry(width=10)
+# Entry
+entry = Entry(width=7)
 # Add some text to begin with
 entry.insert(END, string="0")
 entry.grid(column=1, row=0)
@@ -30,12 +31,12 @@ label1.config(padx=10)
 
 label2 = Label(text="0", font=FONT)
 label2.grid(column=1, row=1)
-# label2.config(padx=10)
 
 label3 = Label(text="Km", font=FONT)
-label3.grid(column=3, row=1)
-# label3.config(padx=10)
+label3.grid(column=2, row=1)
 
-ba
+# Button
+button = Button(text="Calculate", command=button_cliked)
+button.grid(column=1, row=3)
 
 window.mainloop()
