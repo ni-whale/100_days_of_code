f74157ad88ca9d7114edc9f24671e614d32c4299
parsifal <1nibob1@gmail.com>
2021-07-14 07:22:06 +0300

    Creating of Pomodoro timer from scratch.

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -6,17 +7,44 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 CHECK_MARK = "✅"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 FONT = ("Courier", 40, "normal")
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec == 0 or count_sec < 10:
+        count_sec = "0" + str(count_sec)
+    # if count_min == 0 or count_min < 10:
+    #     count_min = "0" + str(count_min)
 
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
@@ -26,7 +54,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(105, 135, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(105, 135, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
 # Labels
@@ -36,8 +64,8 @@ check_mark_label = Label(text="✅", bg=YELLOW, fg=GREEN, font=("Courier", 14, "
 check_mark_label.grid(column=1, row=4)
 
 # Buttons
-start_button = Button(text="Start", bg=PINK, activebackground=GREEN, fg="white", width=5, height=2, font=("Courier", 13,
-                                                                                                          "normal"))
+start_button = Button(text="Start", bg=PINK, activebackground=GREEN, fg="white",
+                      width=5, height=2, font=("Courier", 13,"normal"), command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", bg=PINK, activebackground=GREEN, fg="white", width=5, height=2, font=("Courier", 13,
