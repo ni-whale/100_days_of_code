23c8d893f6dc660c63563537b1890feb55220c11
parsifal <1nibob1@gmail.com>
2021-07-19 17:35:17 +0300

    Finished with GUI password generator.

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import messagebox
+import secrets
+import pyperclip
 
 # ---------------------------- CONSTANTS ------------------------------- #
 NAVY = "#334257"
@@ -9,11 +12,36 @@ FONT = ("Courier", 12, "normal")
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    password = secrets.token_urlsafe(9)
+    pyperclip.copy(password)
+    e_password.delete(0, END)
+    e_password.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def password_saving():
+    website = e_website.get()
+    email = e_email.get()
+    password = e_password.get()
+    info = [website, email, password]
+    if "" in info:
+        messagebox.showerror("Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        result = f"{website} | {email} | {password}"
+        with open("password.txt", mode="a") as file:
+            file.write(f"{result}\n")
+        messagebox.showinfo(title="Record adding", message="The password was successfully added.")
+        data_deleting()
+
+
+def data_deleting():
+    e_website.delete(0, END)
+    e_email.delete(0, END)
+    e_password.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("MyPass")
 window.config(padx=20, pady=20, bg=NAVY)
@@ -46,10 +74,12 @@ e_password = Entry(width=32, bg=GRAY)
 e_password.grid(column=1, row=3, pady=(5, 5), padx=(0, 5))
 
 # Buttons
-b_generate_password = Button(text="Generate", font=FONT, bg=GRAY, activebackground=DARK_BLUE)
+b_generate_password = Button(text="Generate", font=FONT, bg=GRAY, activebackground=DARK_BLUE,
+                             command=password_generator)
 b_generate_password.grid(column=2, row=3, pady=(5, 5))
 
-b_add = Button(text="Add", font=FONT, width=34, bg=GRAY, activebackground=DARK_BLUE)
+b_add = Button(text="Add", font=FONT, width=34, bg=GRAY, activebackground=DARK_BLUE,
+               command=password_saving)
 b_add.grid(column=1, row=4, columnspan=2, pady=(5, 5))
 
 window.mainloop()
