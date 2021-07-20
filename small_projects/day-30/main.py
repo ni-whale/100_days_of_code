81247e3fedce195bc7c4986e6cffcb43fabaef0d
parsifal <1nibob1@gmail.com>
2021-07-20 06:42:56 +0300

    Learning how to proceed with exceptions.

---
+++ small_projects/day-30/main.py
@@ -0,0 +1,66 @@
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} was not found")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("The file was closed.")
+
+# ---------------------------------------------- #
+
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+#
+# if height > 3:
+#     raise ValueError("Human height can not be over 3 meters.")
+#
+# bmi = weight / height ** 2
+# print(bmi)
+
+# ---------------------------------------------- #
+
+# fruits = ["Apple", "Pear", "Orange"]
+#
+# #TODO: Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("Fruit pie")
+#     else:
+#         print(fruit + " pie")
+#
+#
+# make_pie(5)
+
+# ---------------------------------------------- #
+
+# facebook_posts = [
+#     {'Likes': 21, 'Comments': 2},
+#     {'Likes': 13, 'Comments': 2, 'Shares': 1},
+#     {'Likes': 33, 'Comments': 8, 'Shares': 3},
+#     {'Comments': 4, 'Shares': 2},
+#     {'Comments': 1, 'Shares': 1},
+#     {'Likes': 19, 'Comments': 3}
+# ]
+#
+# total_likes = 0
+#
+# for post in facebook_posts:
+#     try:
+#         total_likes = total_likes + post['Likes']
+#     except KeyError:
+#         post['Likes'] = 0
+#
+# print(total_likes)
+
+
+
