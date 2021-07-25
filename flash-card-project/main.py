268efe1305a60fa28242b6c86fa7d6e8823bc60a
parsifal <1nibob1@gmail.com>
2021-07-25 08:45:38 +0300

    Making a logic setup.

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,8 +1,21 @@
 from tkinter import *
-
+import pandas
+import random
 # ---------------------------- CONSTANTS ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ---------------------------- LOGIC SETUP ------------------------------- #
+# def generate_word():
+#     df = pandas.read_csv("data/Words_eng+rus.csv")
+#     data = df.to_dict()
+#     temp = [item for item in data[random.randint(0, 1110)]]
+#     print(temp)
+
+df = pandas.read_csv("data/Words_eng+rus.csv")
+data = df.to_dict()
+word, translation = random.choice(list(data.items()))
+print(f"{word}")
+print(f"{translation}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -13,7 +26,7 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_img)
-c_lang_of_the_word = canvas.create_text(400, 150, text="English", font=("Arial", 30, "italic"))
+c_lang_of_the_word = canvas.create_text(400, 150, text="Title", font=("Arial", 30, "italic"))
 c_word = canvas.create_text(400, 263, text="Word", font=("Arial", 50, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
