5dcfb22224a361df859eefc2b6435564af877bbf
parsifal <1nibob1@gmail.com>
2021-07-30 06:33:54 +0300

    Testing how SMTP is working.

--- Birthday Wisher (Day 32)/main.py
+++ Birthday Wisher (Day 32)/main.py
@@ -10,27 +10,23 @@ recipient = credentials.recipient
 app_password = credentials.app_password  # In case if I want use yahoo as primary email
 
 # --------------------------- LOGIC --------------------------- #
-list_of_quotes = []
-temp = []
 with open("quotes.txt", "r") as data_file:
-    temp.append(data_file.readlines())
-list_of_quotes = [quote for quote in temp]
+    list_of_quotes = data_file.readlines()
+
+
 current_datetime = dt.datetime.now()
+random_quote = random.choice(list_of_quotes)
 
-# if current_datetime.weekday() == 3:  # counts from Monday
-#     with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-#         connection.starttls()
-#         connection.login(user=my_email, password=my_password)
-#         connection.sendmail(
-#             from_addr=my_email,
-#             to_addrs=recipient,
-#             msg=f"Subject:Motivational quote of the day\n\n{random_quote}"
-#         )
+if current_datetime.weekday() == 4:  # counts from Monday
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient,
+            msg=f"Subject:Motivational quote of the day\n\n{random_quote}"
+        )
 
-random_quote = list_of_quotes.index(random.randint(0, 102))
-
-# print(list_of_quotes)
-# print(len(list_of_quotes))
 
 
 
