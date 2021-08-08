9254e05faa5e4d826ef6a8aac8216da333da30e0
parsifal <1nibob1@gmail.com>
2021-08-08 10:28:11 +0300

    Updating an old quiz app from the day 17 by getting questions with API and adding a GUI interface.

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,7 +1,11 @@
 import requests
+from twilio.rest import Client
 
 API_KEY = '61ca6c3f53e8945e36f4b1d0941464e8'
 ONE_CALL_API = 'https://api.openweathermap.org/data/2.5/onecall'
+account_sid = "ACc3edcf1270b5f3e0cd3d209e61921e34"
+auth_token = "7eed7db436acb82dea3947b05b70489c"
+
 
 weather_params = {
     'lat': 47.8508,
@@ -14,4 +18,20 @@ weather_params = {
 response = requests.get(ONE_CALL_API, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+
+will_be_rain = False
+
+for hour in weather_data['hourly'][:12]:  # Made a slice for the next 12 hours
+    if hour['weather'][0]['id'] < 700:  # Everything that less that 700: rain/show/storm and etc.
+        will_be_rain = True
+
+if will_be_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="You better to take an umbrella☂️.",
+        from_='+13126636183',
+        to='+380503224985'
+    )
+    print(message.sid)
+
