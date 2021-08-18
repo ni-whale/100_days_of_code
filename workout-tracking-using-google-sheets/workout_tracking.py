76b89e79d784a446048a59def5044c9be1c6d094
parsifal <1nibob1@gmail.com>
2021-08-18 17:12:05 +0300

    Now is possible to find out the quantity of burned calories by inputting a plain text about the exercise you make.

--- workout-tracking-using-google-sheets/workout_tracking.py
+++ workout-tracking-using-google-sheets/workout_tracking.py
@@ -1,5 +1,30 @@
 from dotenv import load_dotenv
+import os
 import datetime as DT
+import requests
 
 load_dotenv('/home/ni_whale/Documents/Working_space/projects/Python/storage.env')
+
+APP_ID = os.getenv('NUTRITIONIX_APP_ID')
+APP_KEY = os.getenv('NUTRITIONIX_APP_KEY')
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+headers = {
+    'x-app-id': APP_ID,
+    'x-app-key': APP_KEY,
+}
+
+user_input = input("What exercise you did: ")
+
+exercise_params = {
+    "query": user_input
+}
+
+response = requests.post(url=exercise_endpoint, headers=headers, json=exercise_params)
+response.raise_for_status()
+print(response.text)
+
+
+
 
