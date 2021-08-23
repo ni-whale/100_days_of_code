21ec2fb4ac22dc51a9439e91a1c60c4f375705e6
parsifal <1nibob1@gmail.com>
2021-08-23 06:52:16 +0300

    Working with the structure of the project. Trying to get IATA code for each town from the google spreadsheet by using Tequila API & Sheety API.

--- flight-deal-finder/data_manager.py
+++ flight-deal-finder/data_manager.py
@@ -1,12 +1,24 @@
 from dotenv import load_dotenv
+import requests
+import os
 
+#This class is responsible for talking to the Google Sheet.
 
 # ---------------------------- PATHs ------------------------------- #
 load_dotenv('/home/ni_whale/Documents/Working_space/projects/Python/storage.env')
-
 sheety_endpoint_get = "https://api.sheety.co/6f646ab411f639652d7483808719cf20/flightDeals/prices"
 sheety_endpoint_put = "https://api.sheety.co/6f646ab411f639652d7483808719cf20/flightDeals/prices/[Object ID]"
 
+token = os.getenv("SHEETY_BEARER_TOKEN")
+
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.headers_sheety = {f"Authorization": f"Bearer {token}"}
+
+    def getting_list_of_cities(self):
+        sheety_get_response = requests.get(url=sheety_endpoint_get, headers=self.headers_sheety)
+        sheety_get_response.raise_for_status()
+        print(sheety_get_response.json())
+
--- flight-deal-finder/main.py
+++ flight-deal-finder/main.py
@@ -1,2 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
+from data_manager import DataManager
+
+data_manager = DataManager()
+
+data_manager.getting_list_of_cities()
