5a0474ac7e50c0a72b79355e3668159734cb827a
parsifal <1nibob1@gmail.com>
2021-10-04 06:56:55 +0300

    BeautifulSoup intro.

--- bs4/main.py
+++ bs4/main.py
@@ -6,4 +6,17 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
-links = 
+articles = soup.find_all(name="a", class_="storylink")
+articles_texts = []
+articles_links = []
+
+for article_tag in articles:
+    text = article_tag.getText()
+    articles_texts.append(text)
+    link = article_tag.get("href")
+    articles_links.append(link)
+
+article_upvotes = [score.getText() for score in soup.find_all(name="span", class_="score")]
+
+for i in range(len(articles)):
+    print(f"{articles_texts[i]} - {articles_links[i]}, Score: {article_upvotes[i]}")
