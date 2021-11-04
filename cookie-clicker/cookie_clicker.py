8ea10120c803f0b996b59d1de8c5218b1d8847e3
parsifal <1nibob1@gmail.com>
2021-11-04 07:01:10 +0200

    Need to find a way to get the highest result in cookie clicker by using Selenium.

--- cookie-clicker/cookie_clicker.py
+++ cookie-clicker/cookie_clicker.py
@@ -2,21 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from threading import Timer
+import time
 
 service = Service("/home/ni_whale/Documents/Working_space/projects/chromedriver_linux64/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
+current_time = time.time()
+estimated_time = current_time + 60.0
+shopping_time = current_time + 5.0
+
 cookie_button = driver.find_element(By.CSS_SELECTOR, "#cookie")
 
-store = driver.find_elements(By.CSS_SELECTOR, "#store b")
-money = driver.find_element(By.CSS_SELECTOR, "#money").text
+print(f"estimated time = {estimated_time} | shopping_time = {shopping_time}")
 
-for thing in store[:-1:]:  # added slice because split return additional element ['']
-    print(thing.text.split(" - "))
+while time.time() < estimated_time:
+    cookie_button.click()
+    if time.time() > shopping_time:
+        print("time to buy something")
+        shopping_time = time.time() + 5.0
+        print(f"new shopping time = {shopping_time}")
 
-
-
-# driver.quit()
+driver.quit()
