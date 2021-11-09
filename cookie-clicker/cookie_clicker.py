6130d49245f1415ff27427fe1bc54c7ac7c5069c
parsifal <1nibob1@gmail.com>
2021-11-09 07:13:21 +0200

    Non-optimised code get 11.8 cookies per second.

--- cookie-clicker/cookie_clicker.py
+++ cookie-clicker/cookie_clicker.py
@@ -3,19 +3,20 @@ import time
 
 cookie_interface = CookieInterface()
 
-estimated_time = time.time() + 60.0
+estimated_time = time.time() + 60.0 * 5
 shopping_time = time.time() + 5.0
-
-# print(f"estimated time = {estimated_time} | shopping_time = {shopping_time}")
 
 while time.time() < estimated_time:
     cookie_interface.cookie_button_click()
     if time.time() > shopping_time:
+        available_items = []
         for menu_item in cookie_interface.get_cookie_menu():
             if int(cookie_interface.get_money()) >= int(menu_item[1]):
-                print(f"You should buy: {menu_item}")
+                available_items.append(int(menu_item[1]))
+        cookie_interface.buy_item(cookie_interface.get_cookie_menu(), available_items)
         shopping_time = time.time() + 5.0
-        # print(f"new shopping time = {shopping_time}")
+
+cookie_interface.get_cps()
 
 cookie_interface.quit()
 
--- cookie-clicker/cookie_interface.py
+++ cookie-clicker/cookie_interface.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class CookieInterface():
+class CookieInterface:
     def __init__(self):
         service = Service("/home/ni_whale/Documents/Working_space/projects/chromedriver_linux64/chromedriver")
         self.driver = webdriver.Chrome(service=service)
@@ -19,17 +19,25 @@ class CookieInterface():
         cookie_store_menu_list = []
         for position in cookie_store[:-1:]:  # To avoid empty "[]" element in the list
             cookie_store_menu_list.append(position.text.split(" - "))
-
         for price in cookie_store_menu_list:
             price[1] = price[1].replace(",", "")  # removing comas in the price for each element of the menu
-
         return cookie_store_menu_list
+
+    def buy_item(self, cookie_store_menu_list, available_items):
+        for menu_item in cookie_store_menu_list:
+            if int(menu_item[1]) <= max(available_items, default=0):
+                try:
+                    self.driver.find_element(By.CSS_SELECTOR, f"#buy{menu_item[0]}").click()
+                except Exception:
+                    print(f"menu_item = {menu_item} | available_items = {available_items}, max = {max(available_items)}")
 
     def get_money(self):
         money = self.driver.find_element(By.CSS_SELECTOR, "#money").text.replace(",", "")
         return money
 
-
+    def get_cps(self):
+        cps = self.driver.find_element(By.CSS_SELECTOR, "#cps").text
+        print(f"You got {cps}")
 
     def quit(self):
         self.driver.quit()
