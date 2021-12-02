f0848559a1646a4cefb0bd1ff4a4dbe52ce46b7b
parsifal <1nibob1@gmail.com>
2021-12-02 06:47:35 +0200

    Learning how decorators work.

--- higher-lower-game(site version)/server.py
+++ higher-lower-game(site version)/server.py
@@ -1,4 +1,29 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
 
+secret_number = random.randint(0, 9)
+print(secret_number)
+
+@app.route('/')
+def title():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src=https://media.giphy.com/media/dwLk9Qb116F3y/giphy.gif>'
+
+
+@app.route('/<int:number>')
+def random_number_guess(number):
+    if number == secret_number:
+        return '<h1 style="color: green">You found me!</h1>' \
+               '<img src=https://media.giphy.com/media/NohOlMRtBiGcg/giphy.gif>'
+    elif number < secret_number:
+        return '<h1 style="color: red">Too low, try again!</h1>' \
+               '<img src=https://media.giphy.com/media/1xONIE9kieqf4VTX50/giphy.gif>'
+    elif number > secret_number:
+        return '<h1 style="color: purple">Too high, try again!</h1>' \
+               '<img src=https://media.giphy.com/media/oDK8A6xUNjD2M/giphy.gif>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
