696e4ff69e238ae2550f9a10f980dc4e5e6e9ec8
parsifal <1nibob1@gmail.com>
2021-12-09 07:07:16 +0200

    Learning how Flask works + Jinja.

--- web_practice/day-57/server.py
+++ web_practice/day-57/server.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
 from random import randint
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     random_number = randint(1, 10)
-    temp = datetime.now()
-    year = temp.strftime("%Y")
+    year = datetime.now().year
     name = "Nikita B."
-    return render_template("index.html", num=random_number, year=year, name=name)
+    # return render_template("index.html", num=random_number, year=year, name=name)
+
+
+@app.route('/guess/<name>')
+def guess(name):
+    age_result = requests.get(f"https://api.agify.io?name={name}").json()['age']
+    gender_result = requests.get(f"https://api.genderize.io?name={name}").json()['gender']
+
+    return render_template("index.html", name=name.title(), age=age_result, gender=gender_result)
 
 
 if __name__ == '__main__':
