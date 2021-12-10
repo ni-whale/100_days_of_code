69770851b12fcf624387c219dc2c5370a811f1e5
parsifal <1nibob1@gmail.com>
2021-12-10 11:01:09 +0200

    Learning how Flask works + Jinja.

--- web_practice/day-57/server.py
+++ web_practice/day-57/server.py
@@ -12,7 +12,7 @@ def home():
     year = datetime.now().year
     name = "Nikita B."
     # return render_template("index.html", num=random_number, year=year, name=name)
-
+    return "Hello, World!"
 
 @app.route('/guess/<name>')
 def guess(name):
@@ -20,6 +20,14 @@ def guess(name):
     gender_result = requests.get(f"https://api.genderize.io?name={name}").json()['gender']
 
     return render_template("index.html", name=name.title(), age=age_result, gender=gender_result)
+
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    response = requests.get(blog_url)
+    all_posts = response.json()
+
+    return render_template("blog.html", posts=all_posts)
 
 
 if __name__ == '__main__':
