bece4ea6032e44aa6b4a132184f9048aa4d356cc
parsifal <1nibob1@gmail.com>
2021-12-15 07:08:36 +0200

    Learning how Flask works + Jinja.

--- blog-templating/main.py
+++ blog-templating/main.py
@@ -8,17 +8,15 @@ post = Post()
 @app.route('/')
 def home():
     result = post.get_posts()
+    print(result)
     return render_template("index.html", posts=result)
+
 
 @app.route('/post/<post_id>')
 def get_blog(post_id):
-    result = ...
     for uni_post in post.get_posts():
-        if uni_post['id'] == post_id:
-            result = uni_post
-    print(result)
-    return render_template("post.html", post=result)
-
+        if uni_post['id'] == int(post_id):
+            return render_template("post.html", post=uni_post)
 
 
 if __name__ == "__main__":
