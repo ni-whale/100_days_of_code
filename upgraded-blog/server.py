f521b72e72fa6a34a15ad5df6fd43d11ea98cc60
parsifal <1nibob1@gmail.com>
2022-01-13 08:15:43 +0200

    Creating own blog by using bootstrap template + Flask + Ninja

--- upgraded-blog/server.py
+++ upgraded-blog/server.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+blog_url = 'https://api.npoint.io/5c68d12ead0fe6b758e2'
+response = requests.get(blog_url)
+all_posts = response.json()
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+
+
+
 
 
 if __name__ == "__main__":
