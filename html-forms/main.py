7dd14b60243f068a7c55ffac7df9de240c3739d7
parsifal <1nibob1@gmail.com>
2022-01-17 07:00:37 +0200

    Creating own blog by using bootstrap template + Flask + Ninja

---
+++ html-forms/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=["POST"])
+def receive_data():
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
