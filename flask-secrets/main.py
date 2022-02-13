9d5d743d7469242fee2e7d7b39206317161a787e
parsifal <1nibob1@gmail.com>
2022-02-13 08:18:51 +0200

    Creating own blog by using bootstrap template + Flask + Ninja with data input validation.

--- flask-secrets/main.py
+++ flask-secrets/main.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import TextField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class SignupForm(FlaskForm):
-    login = TextField('email')
-    password = TextField('password')
+    email = StringField(label='Email', validators=[Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(8)])
+    submit = SubmitField(label="Log In")
+
 
 app = Flask(__name__)
+app.secret_key = "some secret string"
 
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html', form=SignupForm)
+    form = SignupForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
