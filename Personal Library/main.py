e8b35c1aaa11cfafed85b98de7cc7e340cc90a3b
parsifal <1nibob1@gmail.com>
2022-02-16 08:10:29 +0200

    Creating own blog by using bootstrap template + Flask + Ninja with data input validation.

---
+++ Personal Library/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        all_books.append(
+            {
+                "title": request.form['book_name'],
+                "author": request.form['book_author'],
+                "rating": request.form['rating']
+            }
+        )
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
