973fd20fb9c464389c69cdeb895600ae7165a8da
parsifal <1nibob1@gmail.com>
2022-02-19 08:12:05 +0200

    Testing storing the information in the SQLite DB

--- DB_test/db_test.py
+++ DB_test/db_test.py
@@ -1,6 +1,20 @@
-import sqlite3
+# import sqlite3
+#
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+#
+# # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+#
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.
