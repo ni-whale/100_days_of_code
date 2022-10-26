2d120dd07f4c0ab264a9cb4d1fdd2274ef762a7a
ni_whale <1nibob1@gmail.com>
2022-10-26 09:04:24 +0300

    Review of the course for the KB creation.

--- Flask/Day 57/Combining Jinja templating with APIs/server.py
+++ Flask/Day 57/Combining Jinja templating with APIs/server.py
@@ -1,6 +1,7 @@
 import datetime
+import requests
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,11 +11,18 @@ age = ''
 
 @app.route('/')
 def home():
-    return render_template('index.html')
 
-@app.route('/guess/<name>')
-def name_info():
-    ...
+    return render_template('index.html', name=name, gender=gender, age=age)
+
+@app.route('/guess/<username>')
+def name_info(username):
+    response_age = requests.get(f'https://api.agify.io?name={username}')
+    response_gender = requests.get(f'https://api.genderize.io?name={username}')
+    name = username.capitalize()
+    age = response_age.json()['age']
+    gender = response_gender.json()['gender']
+
+    return render_template('index.html', name=name, gender=gender, age=age)
 
 if __name__ == "__main__":
     app.run(debug=True)
