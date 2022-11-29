37045ae6411569428c4ca368c9d2dd9273a1f38e
ni_whale <1nibob1@gmail.com>
2022-11-29 08:55:17 +0200

    Review of the course for the KB creation.

--- Flask/Day 59/Blog with the bootstrap template/main.py
+++ Flask/Day 59/Blog with the bootstrap template/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import requests
+
 
 app = Flask(__name__)
+blog_data = requests.get('https://api.npoint.io/dc89399832f0790a3177').json()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', posts=blog_data)
+
+@app.route('/post/<post_id>')
+def get_post(post_id):
+    for post in blog_data:
+        if str(post["id"]) == post_id:
+            chosen_post = post
+    return render_template("post.html", post=chosen_post)
 
 
 @app.route('/about')
