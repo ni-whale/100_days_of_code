f518709b6b57e373b1801fb31754a908cb402ba0
ni_whale <1nibob1@gmail.com>
2022-12-06 09:03:52 +0200

    Review of the course for the KB creation.

---
+++ Flask/Day 60/html-forms/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def recieve_data():
+    if request.method == "POST":
+        return f"<h1>Name: {request.form['name']}, Password: {request.form['password']}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
