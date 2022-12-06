c315436857fa30e2f717d58c95bfb8df634e4721
ni_whale <1nibob1@gmail.com>
2022-12-06 09:04:21 +0200

    Review of the course for the KB creation.

---
+++ Flask/Day 60/Solution+&+Completed+Code+day-59-end/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, url_for, redirect
+import requests
+
+posts = requests.get("https://api.npoint.io/dc89399832f0790a3177").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        message = request.form['message']
+        print(f"{name} \ {email} \ {phone_number} \ {message}")
+        return "<h1>Successfully sent your message.</h1>"
+    return render_template("contact.html", message="Contact Me")
+
+@app.route("/form-entry", methods=['GET', 'POST'])
+def receive_data():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        message = request.form['message']
+        print(f"{name} \ {email} \ {phone_number} \ {message}")
+        return "<h1>Successfully sent your message.</h1>"
+    else:
+        return redirect(url_for('contact'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Flask/Day 60/html-forms/main.py
+++ Flask/Day 60/html-forms/main.py
@@ -13,5 +13,6 @@ def recieve_data():
     if request.method == "POST":
         return f"<h1>Name: {request.form['name']}, Password: {request.form['password']}</h1>"
 
+
 if __name__ == "__main__":
     app.run(debug=True)
